1e953cecda05cf3f7871bea7fcb109935c649a22
Avishek-kr <ak251997@gmail.com>
2021-05-05 22:52:49 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
 
 function padTime(time){
   return time.toString().padStart(2, '0');
 }
 
+
+
 export default function App() {
-  const [title,setTile]=useState("Let The Countdown Begins!!");
-  const [timeLeft, SetTimeLeft]= useState(10);  
+  const [title,setTitle]=useState("Let The Countdown Begins!!");
+  const [timeLeft, SetTimeLeft]= useState(15); 
+  const [isRunning, setIsRunning] = useState(false)
+  let intervalRef=useRef(null); 
 
 
   function startTimer(){
-      setInterval(() => {
+    if(intervalRef.current !== null) return
+
+    setTitle(`You're Doing Great!`);
+    setIsRunning(true);
+     intervalRef.current =  setInterval(() => {
           SetTimeLeft(timeLeft => {
           if(timeLeft >= 1) return timeLeft-1;
 
               // reset
+              resetTimer();
           return 0;
           });
       }, 1000);
   }
+  function stopTimer() {
+    if(intervalRef.current  === null) return
+    
+    clearInterval(intervalRef.current);
+    intervalRef.current = null
+    setTitle(`Keep It Up!`);
+    setIsRunning(false)
+  
+  }
 
   function resetTimer(){
-    SetTimeLeft();
+    clearInterval(intervalRef.current);
+    intervalRef.current=null;
+    setTitle(`Ready To Go Another Round?`);
+    SetTimeLeft(25*60) ;
+    setIsRunning(false);
   }
  
   const minutes=padTime(Math.floor(timeLeft/60));
@@ -38,8 +60,8 @@ export default function App() {
       </div>
 
       <div className="buttons">
-        <button onClick={startTimer}>Start</button>
-        <button>Stop</button>
+      {!isRunning &&  <button onClick={startTimer}>Start</button>}
+       {isRunning && <button onClick={stopTimer}>Stop</button>}
         <button onClick={resetTimer}>Reset</button>
       </div>
     </div>
